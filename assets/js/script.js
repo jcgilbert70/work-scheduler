3be307375411d842d0b154a3536bcde65d374826
Jeff <jcgilbert70@gmail.com>
2022-10-27 16:13:15 -0500

    added psuedo code to js and some to html, put framework for each hour section, might need more classes or ID's when starting to code, tried to match up some of the provided css with created divs

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+// need class/ID to reference with $, timer format:  moment().format('dddd, MMMM Do')
+
+//init function to load all locally stored data
+
+// sections for each hour
+
+// input field for each hour section
+
+// a button within each hour section to save entered within input field locally stored
+
+// as moment.js clock passes current hour block, change colors ***  CHECK README FOR COLORS ***
+// need a variable that equal moment.js current hour timer "var XYZ = moment().hour()"? 
